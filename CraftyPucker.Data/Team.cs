608bf7a7c322132451bbe4c96f4c3490027b00bb
codefood <lawrence.anstey@gmail.com>
2016-04-03 12:08:09 +0100

    fixed Team.ToString(), more tests

--- CraftyPucker.Data/Team.cs
+++ CraftyPucker.Data/Team.cs
@@ -26,7 +26,7 @@ namespace CraftyPucker.Data
 
         public override string ToString()
         {
-            return string.Format("{Abbreviation ({Name})", Abbreviation, Name);
+            return string.Format("{0} ({1})", Abbreviation, Name);
         }
 
     }
