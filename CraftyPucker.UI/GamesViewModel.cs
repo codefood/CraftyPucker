0883e8ca771ea309a86c65406529ad5313783b41
Will R <astutecat@gmail.com>
2016-04-03 13:26:46 +0100

    Add a collection view source to support filtering.

---
+++ CraftyPucker.UI/GamesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CraftyPucker.Data;
+using System.Collections.Generic;
+using System.Windows.Data;
+
+namespace CraftyPucker.UI
+{
+    public class GamesViewModel
+    {
+        private readonly Data.DataRepository _gamesRepo;
+
+        public CollectionViewSource CollViewSource { get; set; }
+
+        public IEnumerable<Game> Games => _gamesRepo.Games;
+
+        public GamesViewModel()
+        {
+            _gamesRepo = new Data.DataRepository();
+            CollViewSource = new CollectionViewSource {Source = Games};
+        }
+    }
+}
--- CraftyPucker.UI/MainWindow.xaml.cs
+++ CraftyPucker.UI/MainWindow.xaml.cs
@@ -7,12 +7,12 @@ namespace CraftyPucker.UI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Data.DataRepository _gamesRepo;
+        private readonly GamesViewModel _gamesRepo;
         public MainWindow()
         {
-            _gamesRepo = new Data.DataRepository();
             InitializeComponent();
-            lbGames.ItemsSource = _gamesRepo.Games;
+            _gamesRepo = new GamesViewModel();
+            lbGames.ItemsSource = _gamesRepo.CollViewSource.View;
         }
     }
 }
