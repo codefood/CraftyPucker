cd537c4d9f37aa02f18dbef70fe173073e2d2064
codefood <lawrence.anstey@gmail.com>
2016-04-03 13:06:26 +0100

    actually adding dataRepository because hobofood is stupid

---
+++ CraftyPucker.Data/DataRepository.cs
@@ -0,0 +1,51 @@
+﻿using CraftyPucker.Data.Loaders;
+using CraftyPucker.Data.Sources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftyPucker.Data
+{
+    public class DataRepository
+    {
+
+
+        public IEnumerable<Game> Games
+        {
+            get
+            {
+                if (_games == null)
+                    LoadGames();
+                return _games;
+            }
+            set
+            {
+                _games = value;
+            }
+        }
+
+        public IEnumerable<Team> Teams
+        {
+            get
+            {
+                return this.Games.Select(x => x.HomeTeam)
+                    .Union(this.Games.Select(x => x.AwayTeam))
+                    .Distinct();
+            }
+        }
+
+        private IEnumerable<Game> _games;
+
+        private void LoadGames()
+        {
+            var gameDataSource = new JsonSource();
+            var gameData = gameDataSource.GetData(DateTime.Now);
+            var gameLoader = new JsonGameLoader();
+            _games = gameLoader.Load(gameData);
+        }
+
+
+    }
+}
